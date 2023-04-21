8c9f483ad90d574cf91d670345f9cbb752cb900d
Joshua <23joshtc@gmail.com>
2023-04-20 23:25:44 -0400

    Add capLetters array to java

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Assignment Code
+var capLetters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ];
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +19,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+alert("time to generate password")
+console.log("user will choose yes or no for selcted prompts")
+
+
