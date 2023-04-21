1518ab1c48bb288e99609edf099ae191cf4d16ca
Joshua <23joshtc@gmail.com>
2023-04-20 23:34:00 -0400

    special characters array added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var capLetters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ];
 var lowerLetters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]; 
-
+var specChars=["!", "@", "#","$", "%", "^", "&", "*", "(", ")","?"]
 
 
 
